eec0cff18e0d0ae5103248f60525a4d8536a8d0d
fabien0102 <fabien0102@planee.fr>
2015-04-19 16:55:04 +0200

    Turn display sprite

--- EyeMinion/eye/eye.ino
+++ EyeMinion/eye/eye.ino
@@ -23,37 +23,38 @@ pixel_t adjust_pixel_luminosity(pixel_t pixel_in)
 }
 
 uint8_t reverse_table [52][2] {
-            {7,2},{7,3},{7,4},{7,5},
-      {6,6},{6,5},{6,4},{6,3},{6,2},{6,1},
-{5,0},{5,1},{5,2},{5,3},{5,4},{5,5},{5,6},{5,7},
-{4,7},{4,6},{4,5},{4,4},{4,3},{4,2},{4,1},{4,0},
-{3,0},{3,1},{3,2},{3,3},{3,4},{3,5},{3,6},{3,7},
-{2,7},{2,6},{2,5},{2,4},{2,3},{2,2},{2,1},{2,0},
-      {1,1},{1,2},{1,3},{1,4},{1,5},{1,6},
-            {0,5},{0,4},{0,3},{0,2}
+            {2,0},{3,0},{4,0},{5,0},
+      {6,1},{5,1},{4,1},{3,1},{2,1},{1,1},
+{0,2},{1,2},{2,2},{3,2},{4,2},{5,2},{6,2},{7,2},
+{7,3},{6,3},{5,3},{4,3},{3,3},{2,3},{1,3},{0,3},
+{0,4},{1,4},{2,4},{3,4},{4,4},{5,4},{6,4},{7,4},
+{7,5},{6,5},{5,5},{4,5},{3,5},{2,5},{1,5},{0,5},
+      {1,6},{2,6},{3,6},{4,6},{5,6},{6,6},
+            {5,7},{4,7},{3,7},{2,7}
 };
 
 /**
  * Display a sprite
  *
  * @param sprite_t *sprite
- * @param uint8_t [offset_x = 0]
- * @param uint8_t [offset_y = 0]
- * @param uint8_t [loop = 0]
+ * @param int [offset_x = 0]
+ * @param int [offset_y = 0]
+ * @param bool [loop = false]
  **/
-void display_sprite(sprite_t *sprite, uint8_t offset_x = 0, uint8_t offset_y = 0, uint8_t loop = 0)
+void display_sprite(sprite_t *sprite, int offset_x = 0, int offset_y = 0, bool loop = false)
 {
     for (int i=0; i<LED_COUNT_PER_EYE; i++) {
         pixel_t pix;
-        bool is_in_matrice = (reverse_table[i][0] + offset_y >= 0 && reverse_table[i][0] + offset_y < LINE_COUNT
-                      && reverse_table[i][1] + offset_x >= 0 && reverse_table[i][1] + offset_x < ROW_COUNT);
+        int x = reverse_table[i][0] - offset_y;
+        int y = reverse_table[i][1] - offset_x;
+        bool is_in_matrice = (y >= 0 && y < LINE_COUNT && x >= 0 && x < ROW_COUNT);
 
         if (loop || is_in_matrice) {
-            pix = (*sprite)[(reverse_table[i][0] + offset_y) % LINE_COUNT][(reverse_table[i][1] + offset_x) % ROW_COUNT];
-            pix = adjust_pixel_luminosity(pix);
+            pix = (*sprite)[x % LINE_COUNT][y % ROW_COUNT];
         } else {
             pix = 0;
         }
+
         pix = adjust_pixel_luminosity(pix);
         leds.setPixel(i, pix);
     }
