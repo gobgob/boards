ddad16fd902648a3bf589b5d68ab64db9727c110
fabien0102 <fabien0102@planee.fr>
2015-04-26 18:58:54 +0200

    Define eyes ports and leds count

--- EyeMinion/eye/eye.h
+++ EyeMinion/eye/eye.h
@@ -9,6 +9,11 @@
 #define LINE_COUNT 8
 #define ROW_COUNT 8
 
+#define EYE1_PORT 1 // PIN 2
+#define EYE2_PORT 3 // PIN 7
+
+#define LED_COUNT LED_COUNT_PER_EYE * MAX(EYE1_PORT, EYE2_PORT)
+
 typedef uint32_t pixel_t;
 typedef pixel_t sprite_t[ROW_COUNT][LINE_COUNT];
 
