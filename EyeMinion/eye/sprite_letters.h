2c9264a90ce96ab2c3d3e8658e331f460065c137
Mathieu Cassard <cassard.mathieu@gmail.com>
2015-04-26 14:41:56 +0200

    Improve sprites and letters memory management
    Signed-off-by: Mathieu Cassard <cassard.mathieu@gmail.com>

---
+++ EyeMinion/eye/sprite_letters.h
@@ -0,0 +1,6 @@
+#ifndef _LETTERS_H_
+#define _LETTERS_H_
+
+extern sprite_t * letters[256];
+
+#endif //_LETTERS_H_
---
+++ EyeMinion/eye/sprites.h
@@ -0,0 +1,12 @@
+#ifndef _SPRITES_H_
+#define _SPRITES_H_
+
+extern const sprite_t arrow_top;
+extern const sprite_t heart;
+extern const sprite_t clap_open;
+extern const sprite_t clap_close;
+
+extern const sprite_t sprite_row;
+extern const sprite_t sprite_line;
+
+#endif
